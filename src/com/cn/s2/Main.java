e762922665d58c7d9e66c5fb42c298fba8ad6677
lxt <1795737071@qq.com>
2026-06-27 14:14:29 +0800

    测试2

--- src/com/cn/s2/Main.java
+++ src/com/cn/s2/Main.java
@@ -20,7 +20,7 @@ public class Main {
                 System.out.println(Thread.currentThread().getName() + " 正在处理文件：" + fileId);
             });
         }
-
+        System.out.println(1);
         // 关闭线程池
         pool.shutdown();
     }
--- src/com/cn/s2/Person.java
+++ src/com/cn/s2/Person.java
@@ -1,4 +1,7 @@
 package com.cn.s2;
 
 public class Person {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
 }
