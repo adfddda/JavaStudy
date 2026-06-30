948c254bb18d67296e86f16d7aa0ea01a8c42403
lxt <1795737071@qq.com>
2026-06-30 17:33:26 +0800

    打印6

--- src/com/cn/login/Login.java
+++ src/com/cn/login/Login.java
@@ -4,5 +4,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("处理登录请求");
         System.out.println("修复bug");
+        System.out.println(6);
     }
 }
