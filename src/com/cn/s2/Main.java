08b86deafdef38c9c685e31f8061d8fa21f70685
lxt <1795737071@qq.com>
2026-07-01 16:37:14 +0800

    打印1000

--- src/com/cn/s2/Main.java
+++ src/com/cn/s2/Main.java
@@ -21,7 +21,7 @@ public class Main {
             });
         }
         System.out.println(1);
-        System.out.println(3);
+        System.out.println(1000);
         // 关闭线程池
         pool.shutdown();
     }
